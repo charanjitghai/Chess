4becac66d86b024165c4f4bb41174f1e13039317
charanjitghai <ghaicharanjit@gmail.com>
2017-05-27 00:26:51 -0700

    Vertical Move code

---
+++ src/VerticalMove.java
@@ -0,0 +1,46 @@
+/**
+ * Created by cghai on 5/26/17.
+ */
+public class VerticalMove implements Move{
+
+    private static VerticalMove instance = null;
+    private VerticalMove(){
+
+    }
+    public static VerticalMove getInstance(){
+        if(instance == null)
+            instance = new VerticalMove();
+        return instance;
+    }
+
+    public boolean isValidMove(Square source, Square destination, Board board){
+
+        int sourceRow = source.getRow();
+        char sourceCol = source.getCol();
+
+        int destRow = destination.getRow();
+        char destCol = destination.getCol();
+
+        if(sourceCol != destCol )
+            return false;
+
+
+        int step;
+        if(sourceRow < destRow)
+            step = 1;
+        else if(sourceRow > destRow)
+            step = -1;
+        else
+            return false;
+
+        for(int row = sourceRow + step; row != destRow ; row += step){
+            Square square = board.getSquare(sourceCol, row);
+            if(square.getPeice() != null)
+                return false;
+        }
+
+        return true;
+
+    }
+
+}
